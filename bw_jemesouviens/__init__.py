d08a1f17357e92bde2845300f359b8ff219c5ea7
marledej <m.dejong2@uu.nl>
2024-05-23 22:31:04 +0200

    first commit

---
+++ bw_jemesouviens/__init__.py
@@ -0,0 +1,8 @@
+"""bw_jemesouviens."""
+
+__all__ = (
+    "__version__",
+    # Add functions and variables you want exposed in `bw_jemesouviens.` namespace here
+)
+
+__version__ = "0.0.1"
---
+++ tests/conftest.py
@@ -0,0 +1,3 @@
+"""Fixtures for bw_jemesouviens"""
+
+# import pytest
